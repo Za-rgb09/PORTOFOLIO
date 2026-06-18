6d9d5eb370bec29d7b8a48caea8473683d61b534
AliyaZahra-rgb <pklpst2@gmail.com>
2026-06-18 11:48:08 +0700

    add requirements

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8&l=p*9gteo%hlj4%6n0@kls=g&vdtd0@3#lpwi(5d(d&=6wrj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
